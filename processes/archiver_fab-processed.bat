d1f3646fb4bdbd0b38b2b7962795e7fc1a4a8245
Bret Combe <bret.combe@gmail.com>
2022-08-23 23:25:47 -0600

    logging adjustments

--- processes/archiver_fab-processed.bat
+++ processes/archiver_fab-processed.bat
@@ -1,5 +1,5 @@
 :: Archive certain directories.
-robocopy \\10.15.30.51\Departments\Fabrication\Public\External\Processed \\10.15.30.51\Departments\Fabrication\Public\External\Processed_Archive /move /s /MINAGE:14
+robocopy \\10.15.30.51\Departments\Fabrication\Public\External\Processed \\10.15.30.51\Departments\Fabrication\Public\External\Processed_Archive /move /s /MINAGE:14 /log+:\\10.15.30.51\Departments\Prepress\Support\Logs\CutVinyl.txt
 
 :: Move old archived directories to the trash to be deleted.
 robocopy \\10.15.30.51\Departments\Fabrication\Public\External\Processed_Archive \\10.15.30.51\Departments\Prepress\Support\Archives\Trash /move /s /MINAGE:90
