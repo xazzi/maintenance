42c250c8cd1e35b185764fc10150dd0bf44ed758
Bret Combe <bret.combe@gmail.com>
2023-03-02 16:55:04 +0000

    updates

--- processes/louisville_transfer-to-rip.bat
+++ processes/louisville_transfer-to-rip.bat
@@ -1,14 +1,14 @@
 :: Press A
-robocopy \\10.15.30.51\M\Prepress\Support\Fiery\LOU\PressA "\\10.1.1.143\Hotfolders\Press A New" /move /s
+robocopy \\10.15.30.51\M\Prepress\Support\Fiery\LOU\PressA "\\10.1.1.143\Hotfolders\022423A" /move /s
 
 :: Move old Press A files to the trash to be deleted.
-robocopy "\\10.1.1.143\Hotfolders\Press A New" \\10.15.30.51\M\Prepress\Support\Archives\Trash /move /s /MINAGE:7
+robocopy "\\10.1.1.143\Hotfolders\022423A" \\10.15.30.51\M\Prepress\Support\Archives\Trash /move /s /MINAGE:7
 
 :: Press B
-robocopy \\10.15.30.51\M\Prepress\Support\Fiery\LOU\PressB "\\10.1.1.143\Hotfolders\Press A New" /move /s
+robocopy \\10.15.30.51\M\Prepress\Support\Fiery\LOU\PressB "\\10.1.1.143\Hotfolders\022423B" /move /s
 
-:: Move old files to the trash to be deleted.
-robocopy "\\10.1.1.143\Hotfolders\Press B Back" \\10.15.30.51\M\Prepress\Support\Archives\Trash /move /s /MINAGE:7
+:: Move old Press B files to the trash to be deleted.
+robocopy "\\10.1.1.143\Hotfolders\022423B" \\10.15.30.51\M\Prepress\Support\Archives\Trash /move /s /MINAGE:7
 
 ping 10.15.30.51 -n 6 > nul
 exit
