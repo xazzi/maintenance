a39f1a377633e4596390cf4dfc021caf2a486e0d
Bret Combe <bret.combe@gmail.com>
2023-08-21 12:06:39 -0700

    changed path for wxm

--- processes/transfer_wixom.bat
+++ processes/transfer_wixom.bat
@@ -1,8 +1,8 @@
 :: Transfer the print files
-robocopy C:\Switch\Transfer\Wixom\Print "W:\_Incoming Wixom Production Files - All\Print" /move /s
+robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Print" "W:\_Incoming Wixom Production Files - All\Print" /move /s
 
 :: Transfer the cut files
-robocopy C:\Switch\Transfer\Wixom\Cut "W:\_Incoming Wixom Production Files - All\Cut" /move /s
+robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Cut" "W:\_Incoming Wixom Production Files - All\Cut" /move /s
 
 ping 10.15.30.51 -n 6 > nul
 exit
