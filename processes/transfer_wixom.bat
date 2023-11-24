c4c155306535f8813398faf0dc9bfc80743d9d8a
Bret Combe <bret.combe@gmail.com>
2023-11-24 09:26:28 -0800

    set WXM emergency

--- processes/transfer_wixom.bat
+++ processes/transfer_wixom.bat
@@ -1,8 +1,10 @@
 :: Transfer the print files
-robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Print" "W:\_Incoming Wixom Production Files - All\Print" /move /s
+:: robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Print" "W:\_Incoming Wixom Production Files - All\Print" /move /s
+robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Print" "\\10.65.100.241\shareddata\Emergency\Print" /move /s
 
 :: Transfer the cut files
-robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Cut" "W:\_Incoming Wixom Production Files - All\Cut" /move /s
+:: robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Cut" "W:\_Incoming Wixom Production Files - All\Cut" /move /s
+robocopy "\\10.15.30.51\m\Prepress\Support\file-transfer\Wixom\Cut" "\\10.65.100.241\shareddata\Emergency\Cut" /move /s
 
 ping 10.15.30.51 -n 6 > nul
 exit
