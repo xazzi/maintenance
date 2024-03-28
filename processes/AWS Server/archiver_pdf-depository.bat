3cf25f24b24e98f653f85036b0e7300a7f576a49
Bret Combe <bret.combe@gmail.com>
2024-03-28 16:23:36 +0000

    pdfDepo change

--- processes/AWS Server/archiver_pdf-depository.bat
+++ processes/AWS Server/archiver_pdf-depository.bat
@@ -1,5 +1,5 @@
 :: Move files to the .AutomationTrash folder to be deleted.
-robocopy "\\amz-phoenix-p02\pdfDepository" "\\AMZ-IMPSW-DATA\IMPSW_DATA\.Live Services\To Delete" /mov /MINAGE:5
+robocopy "\\amz-phoenix-p02\pdfDepository" "\\AMZ-IMPSW-DATA\IMPSW_DATA\.Live Services\To Delete" /mov /MINAGE:3
 
 ping 10.15.30.51 -n 6 > nul
 exit
