bd3596ffb25106b606088a38dc0efe2ed08f5dd7
Bret Combe <bret.combe@gmail.com>
2024-10-29 12:49:32 -0700

    agfa fix

--- processes/SLC Server/archiver_dev-backup.bat
+++ processes/SLC Server/archiver_dev-backup.bat
@@ -1,5 +1,6 @@
 :: Delete certain directories.
 robocopy "\\slc-stor-01.us.digitalroominc.com\Process_Development\Backup" "\\amz-impsw-data.us.digitalroominc.com\IMPSW_DATA\Backup" /move /s
+robocopy "C:\Switch\.Live Services\.DoNotDelete" "\\slc-stor-01.us.digitalroominc.com\Process_Development\Backup" /purge
 
 ping slc-stor-01 -n 6 > nul
 exit
