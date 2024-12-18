ff7bc7d86f4efd961547ad674030a988ed62d1a4
Bret Combe <bret.combe@gmail.com>
2024-12-18 14:37:12 -0800

    agfa fix

--- processes/SLC Server/hotfolder-fix_agfa.bat
+++ processes/SLC Server/hotfolder-fix_agfa.bat
@@ -1,5 +1,5 @@
 :: Move files to the Zund hotfolders.
-robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\Hotfolder Fix\Agfa" "\\10.15.35.123\HotFolderRoot" /mov /s
+robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\Hotfolder Fix\Agfa" "\\10.15.32.8\HotFolderRoot" /mov /s
 
 ping slc-stor-01 -n 6 > nul
 exit
