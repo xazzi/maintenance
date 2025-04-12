ff44ef06c1f92bd673ae181380b2ce19641e413c
Bret Combe <bret.combe@gmail.com>
2025-04-12 10:09:19 -0700

    updates

--- processes/SLC/flag-for-deletion.bat
+++ processes/SLC/flag-for-deletion.bat
@@ -3,7 +3,6 @@ robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\Autorip" "\\
 robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\Processed" "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\To Delete" /move /s /MINAGE:7
 robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Archives\Fabrication\To Cut" "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\To Delete" /move /s /MINAGE:90
 robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Archives\Fabrication\Cut Vinyl" "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\To Delete" /move /s /MINAGE:90
-robocopy "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\Processed" "\\slc-stor-01.us.digitalroominc.com\Production\Imposition\.Live Services\To Delete" /move /s /MINAGE:7
 
 ping slc-stor-01 -n 6 > nul
 exit
